2301b668df51108b52c44ce972996309d2917b97
Yago Hoffmann <yagoha1996@gmail.com>
2020-01-03 20:32:09 -0300

    Atualizada mensagem de despedida

--- TesteRede/Program.cs
+++ TesteRede/Program.cs
@@ -7,7 +7,7 @@ namespace TesteRede
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Goodby cruel world");
+            Console.WriteLine("Goodby !");
             Console.ReadLine();
         }
     }
